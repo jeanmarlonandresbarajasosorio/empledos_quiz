3736068afffba8141d344a662d8f9a5deea83b8c
jeanmarlonandresbarajasosorio <86082644+jeanmarlonandresbarajasosorio@users.noreply.github.com>
2025-05-23 12:34:02 -0500

    feat: :sparkles: agrego registro final

---
+++ listadergistro/registro.sql
@@ -0,0 +1,12 @@
+CREATE table tabla_de_empleados;
+(SELECT idempleados_id);
+
+
+CREATE table nombre_de_empleado
+(SELECT nombre_de_empleado) (40)
+
+CREATE table apellido_de_empleado
+(SELECT apellido_de_empleado) (30)
+
+CREATE table correo_de_empleados
+(SELECT correo_de_empleados)
